e1d832b103d47af428d9b5194e79762ca939b598
ckkannan <ck_kannan@yahoo.com>
2023-03-12 14:08:34 -0400

    first commit

--- TableQueryOrg.go
+++ TableQueryOrg.go
@@ -6,7 +6,14 @@ import (
 	"net/http"
 )
 
-func (c *Client) GetRows() (map[string]interface{}, error) {
+type rowOrg struct {
+	Sys_id      string `json:"sys_id"`
+	To_adgroup  string `json:"to_adgroup"`
+	To_org_name string `json:"to_org_name"`
+	To_org_type string `json:"to_org_type"`
+}
+
+func (c *Client) GetOrgRows() ([]rowOrg, error) {
 	url := fmt.Sprintf("%s/api/now/table/%s?sysparm_query=%s&sysparm_display_value=true", c.sn_url, c.Table, c.Query)
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
@@ -19,12 +26,12 @@ func (c *Client) GetRows() (map[string]interface{}, error) {
 		return nil, err
 	}
 	// fmt.Println(url)
-	var datarows map[string]interface{}
-	err = json.Unmarshal([]byte(body), &datarows)
+	rowOrgs := []rowOrg{}
+	err = json.Unmarshal([]byte(body), &rowOrgs)
 	if err != nil {
 		fmt.Println("Failed Json")
 		return nil, err
 	}
-	return datarows, nil
+	return rowOrgs, nil
 
 }
